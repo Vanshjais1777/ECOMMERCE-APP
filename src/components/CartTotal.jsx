16e1dd57ebf4c1406810dd663b2d44918c01f3fe
vansh jaiswal <vanshjais1777@gmail.com>
2024-09-23 21:07:50 +0530

    c

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -11,7 +11,6 @@ const CartTotal = () => {
             <div className='text-2xl'>
                 <Title text1={'CART'} text2={'TOTALS'} />
             </div>
-
             <div className='flex flex-col gap-2 mt-2 text-sm'>
                 <div className='flex justify-between'>
                     <p>SubTotal</p>
